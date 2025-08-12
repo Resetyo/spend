2e3c1ac3ea3db6c9be3183524259cf15074836fd
Victor <ostorodonodor@gmail.com>
2025-08-12 23:41:29 +0700

    Capfile lock 3.19.2

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,5 +1,5 @@
 # config valid for current version and patch releases of Capistrano
-lock "~> 3.14.1"
+lock "~> 3.19.2"
 
 set :application, "spend"
 set :repo_url, "git@github.com:Resetyo/spend.git"
