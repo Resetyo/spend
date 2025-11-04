ec7006792074f7ce2568dc408daee13fe23ec7a7
Victor <ostorodonodor@gmail.com>
2025-11-04 15:03:55 +0700

    exclude targets, raise if same target

--- app/controllers/spends_controller.rb
+++ app/controllers/spends_controller.rb
@@ -13,7 +13,9 @@ class SpendsController < ApplicationController
     @categories = Category.pluck(:title, :id)
     @sources = Source.pluck(:id, :title).to_h
     first_spend = @spends.first&.created_at
-    @last_time = first_spend && first_spend.day < Time.zone.now.day ? first_spend + 1.minute : nil
+    @last_time = if first_spend && first_spend.day < Time.zone.now.day || first_spend.month < Time.zone.now.month
+                   first_spend + 1.minute
+                 end
   end
 
   def total
@@ -25,14 +27,16 @@ class SpendsController < ApplicationController
                               .transform_values do |group_by_date|
       group_by_date.group_by { |item| @categories.find { |category| category.last == item.category_id }&.first }
                    .transform_values do |group_by_category|
-        group_by_category.sum { |item| item.amount > 0 ? item.amount : 0 }
+        group_by_category.sum do |item|
+          item.amount > 0 && !item.target_id ? item.amount : 0
+        end
       end
     end
 
     @grouped_earnings = SpendItem.all
                                  .group_by { |item| item.created_at.strftime('%m-%y') }
                                  .transform_values do |group_by_date|
-      group_by_date.sum { |item| item.amount < 0 ? -item.amount : 0 }
+      group_by_date.sum { |item| item.amount < 0 && !item.target_id ? -item.amount : 0 }
     end
 
     date = params[:date].present? && Time.zone.parse(params[:date])
@@ -42,6 +46,12 @@ class SpendsController < ApplicationController
   def create
     @spend_item = SpendItem.new(spend_item_params)
     @spend_item.like_id = spend_item_params[:like].to_i == 1 ? current_user.id : nil
+
+    if spend_item_params[:source_id] == spend_item_params[:target_id]
+      flash[:notice] = 'перевод на источник'
+      redirect_to(spends_path)
+      return
+    end
 
     if @spend_item.save
       redirect_to spends_path
